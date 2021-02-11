8b6f0ccb4ae1be85cd6bbdf254c105d18789b0cc
Serhii Belei <sergiy.beley@gmail.com>
2021-02-11 20:07:16 +0200

    consumer

--- src/main/java/org/crc83/kafkatoelastic/KafkatoelasticApplication.java
+++ src/main/java/org/crc83/kafkatoelastic/KafkatoelasticApplication.java
@@ -1,13 +1,27 @@
 package org.crc83.kafkatoelastic;
 
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.kafka.annotation.KafkaListener;
+
+import java.util.concurrent.CountDownLatch;
 
 @SpringBootApplication
 public class KafkatoelasticApplication {
+
+	public static Logger logger = LoggerFactory.getLogger(KafkatoelasticApplication.class);
+	private final CountDownLatch latch = new CountDownLatch(3);
 
 	public static void main(String[] args) {
 		SpringApplication.run(KafkatoelasticApplication.class, args);
 	}
 
+	@KafkaListener(topics = "dev", groupId = "default")
+	public void listen(ConsumerRecord<?, ?> cr) throws Exception {
+		logger.info(cr.toString());
+		latch.countDown();
+	}
 }
