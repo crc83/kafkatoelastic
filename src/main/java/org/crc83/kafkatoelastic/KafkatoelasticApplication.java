7619ea880619aa5733f6984dbf9d1f429931db67
Serhii Belei <sergiy.beley@gmail.com>
2021-02-11 14:15:40 +0200

    Initial cvommit

---
+++ src/main/java/org/crc83/kafkatoelastic/KafkatoelasticApplication.java
@@ -0,0 +1,13 @@
+package org.crc83.kafkatoelastic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkatoelasticApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkatoelasticApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/crc83/kafkatoelastic/KafkatoelasticApplicationTests.java
@@ -0,0 +1,13 @@
+package org.crc83.kafkatoelastic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkatoelasticApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
